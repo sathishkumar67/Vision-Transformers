0651503ac0ba3a8129f78467d56f797536232e2b
Sathish Kumar R <90573412+sathishkumar67@users.noreply.github.com>
2024-07-10 20:45:01 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-from modules import *
-
-transform = transforms.Compose([
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor()
-])
